e7a920d40e6cd954a0f8a732931ea2ca1b956929
professor nazil <nazilsheikh.emerson@gmail.com>
2024-10-07 15:22:25 +0530

    Update views.py

--- DjangoApplication/ml_app/views.py
+++ DjangoApplication/ml_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 import torch
 import torchvision
-from torchvision import transforms, models
+from torchvision import transforms, models 
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 import os
