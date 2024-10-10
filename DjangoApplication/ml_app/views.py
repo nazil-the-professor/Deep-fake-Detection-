9aa8a683098122302b6c62dee1e23079c460c746
professor nazil <nazilsheikh.emerson@gmail.com>
2024-10-10 18:08:04 +0530

    Update views.py

--- DjangoApplication/ml_app/views.py
+++ DjangoApplication/ml_app/views.py
@@ -5,7 +5,7 @@ from torchvision import transforms, models
 from torch.utils.data import DataLoader   
 from torch.utils.data.dataset import Dataset   
 import os   
-import numpy as np     
+import numpy as np      
 import cv2     
 import matplotlib.pyplot as plt   
 import face_recognition  
